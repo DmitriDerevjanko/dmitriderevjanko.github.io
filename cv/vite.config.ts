a3483c8d52540810a31096727d56b48418cecea6
derevjankodmitri <dmitri.derevjanko02@gmail.com>
2024-01-14 11:06:50 +0200

    initial commit

---
+++ cv/vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  base: './', // Используйте относительный путь для корректной работы на GitHub Pages
+});
